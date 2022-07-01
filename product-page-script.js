811ece6ad35b89f592baafe8245ddbf8da10c014
unknown <Gregory@MSI.(none)>
2022-07-01 09:27:53 -0400

    july 1 launch test new domain

--- product-page-script.js
+++ product-page-script.js
@@ -1,28 +1,4 @@
-$('.carousel').on('touchstart', function(event){
-  const xClick = event.originalEvent.touches[0].pageX;
-  $(this).one('touchmove', function(event){
-      const xMove = event.originalEvent.touches[0].pageX;
-      const sensitivityInPx = 5;
 
-      if( Math.floor(xClick - xMove) > sensitivityInPx ){
-          $(this).carousel('next');
-      }
-      else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
-          $(this).carousel('prev');
-      }
-  });
-  $(this).on('touchend', function(){
-      $(this).off('touchmove');
-  });
-});
-
-$('#carouselExampleIndicators').bind('mousewheel', function(e) {
-  if(e.originalEvent.wheelDelta /120 > 0) {
-      $(this).carousel('next');
-  } else {
-      $(this).carousel('prev');
-  }
-});
 
 // Get the container element
 var btnContainer = document.getElementById("image-selector");
